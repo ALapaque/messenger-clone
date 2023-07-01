9a293e3cb5c8e57ab84a8c31821344462f2c803c
amaury <amaury.lapaque@gmail.com>
2023-07-01 19:28:08 +0200

    added DesktopSidebar.tsx && MobileNavigation.tsx

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,11 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt"
+import NextAuth, { AuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
 import prisma from "@messenger-clone/app/libs/prismadb"
-import NextAuth, {AuthOptions} from "next-auth";
-import {PrismaAdapter} from "@next-auth/prisma-adapter";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: AuthOptions = {
 	adapter: PrismaAdapter(prisma),
@@ -20,44 +21,44 @@ export const authOptions: AuthOptions = {
 		CredentialsProvider({
 			name: 'credentials',
 			credentials: {
-				email: {label: "Email", type: "email"},
-				password: {label: "Password", type: "password"}
+				email: { label: 'email', type: 'text' },
+				password: { label: 'password', type: 'password' }
 			},
 			async authorize(credentials) {
 				if (!credentials?.email || !credentials?.password) {
-					throw new Error('Invalid credentials')
+					throw new Error('Invalid credentials');
 				}
 
 				const user = await prisma.user.findUnique({
 					where: {
 						email: credentials.email
 					}
-				})
+				});
 
 				if (!user || !user?.hashedPassword) {
-					throw new Error('Invalid credentials')
+					throw new Error('Invalid credentials');
 				}
 
 				const isCorrectPassword = await bcrypt.compare(
 					credentials.password,
 					user.hashedPassword
-				)
+				);
 
 				if (!isCorrectPassword) {
-					throw new Error('Invalid credentials')
+					throw new Error('Invalid credentials');
 				}
 
-				return user
+				return user;
 			}
 		})
 	],
 	debug: process.env.NODE_ENV === 'development',
 	session: {
-		strategy: 'jwt',
+		strategy: "jwt",
 	},
-	secret: process.env.NEXTAUTH_SECRET
+	secret: process.env.NEXTAUTH_SECRET,
 }
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
